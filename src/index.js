7745453bdb9128610725164f5cfb4d0ce90fac70
Eric Van <ggclarence@yahoo.com>
2022-02-09 00:34:44 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector('form')
+let taskInput = document.getElementById(`new-task-description`)
+let taskList = document.getElementById(`tasks`)
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addToDo()
+  
+    form.reset();
+       })
+
+
+
+  function addToDo(){
+        let li = document.createElement(`li`)
+        li.innerText = taskInput.value
+        taskList.appendChild(li);
+
+        let btn = document.createElement(`button`);
+        btn.textContent = 'x'
+        li.appendChild(btn)
+        btn.addEventListener(`click`, () => {
+        li.remove();
+    })
+  }
+})
+
+
+
+
+
+
+
+
+
